d5cf8ef6d7ed292de1f3b60d0f6495a7a6f375c4
Andreas Steffan <a.steffan@contentreich.de>
2024-09-29 16:54:34 +0200

    feat: Introduced oh-my-tmux

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,2 +1,12 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+-- https://neovim.io/doc/user/nvim.html#nvim-from-vim
+
+-- Set the runtimepath
+-- vim.o.runtimepath = vim.o.runtimepath .. ",~/.vim,~/.vim/after"
+
+-- Set packpath to the same as runtimepath
+-- vim.o.packpath = vim.o.runtimepath
+
+-- Source the .vimrc file
+-- vim.cmd("source ~/.vimrc")
