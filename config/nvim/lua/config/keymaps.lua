f733d115ff3fda39b0470752a37a6c9bcd9d8cdd
Andreas Steffan <a.steffan@contentreich.de>
2025-02-12 11:50:03 +0100

    feat(nvim): Harpoon added

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- lazy.nvim keys
+
+-- Quick chat with Copilot: Keybinding works. However "Quick Chat" is bound already
+--vim.keymap.set("n", "<leader>ccq", function()
+--  local input = vim.fn.input("Quick Chat: ")
+--  if input ~= "" then
+--    require("CopilotChat").ask(input, { selection = require("CopilotChat.select").buffer })
+--  end
+-- end, { desc = "CopilotChat - Quick chat" })
