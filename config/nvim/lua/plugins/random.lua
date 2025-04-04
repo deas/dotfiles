791dec052e46e024cf596fcbd61a3267ce9eaaa2
Andreas Steffan <a.steffan@contentreich.de>
2025-04-04 21:25:40 +0200

    feat: hcloud completion + avante / lua nvim config

--- config/nvim/lua/plugins/random.lua
+++ config/nvim/lua/plugins/random.lua
@@ -6,7 +6,61 @@
 -- * override the configuration of LazyVim plugins
 --
 -- See example.lua for more examples
---
+
+-- Alternative lua debug adapter
+-- https://tamerlan.dev/a-guide-to-debugging-applications-in-neovim/
+--[[ 
+local dap = require("dap")
+-- Adapters
+dap.adapters["local-lua"] = {
+  type = "executable",
+  command = "node",
+  args = {
+    "/home/deas/work/projects/3rd-party/local-lua-debugger-vscode/extension/debugAdapter.js",
+  },
+  enrich_config = function(config, on_config)
+    if not config["extensionPath"] then
+      local c = vim.deepcopy(config)
+      -- 💀 If this is missing or wrong you'll see
+      -- "module 'lldebugger' not found" errors in the dap-repl when trying to launch a debug session
+      c.extensionPath = "/home/deas/work/projects/3rd-party/local-lua-debugger-vscode"
+      on_config(c)
+    else
+      on_config(config)
+    end
+  end,
+}
+
+-- Configurations
+dap.configurations = {
+  lua = {
+    {
+      name = "Current file (local-lua-dbg, lua)",
+      type = "local-lua",
+      repl_lang = "lua",
+      request = "launch",
+      cwd = "${workspaceFolder}",
+      program = {
+        lua = "luajit",
+        file = "${file}",
+      },
+      args = {},
+    },
+    {
+      name = "Current file (local-lua-dbg, neovim lua interpreter with nlua)",
+      type = "local-lua",
+      repl_lang = "lua",
+      request = "launch",
+      cwd = "${workspaceFolder}",
+      program = {
+        lua = "nlua",
+        file = "${file}",
+      },
+      args = {},
+    },
+  },
+}
+--]]
 -- Hide the HUD log popup that is otherwise shown when Conjure connects to the REPL process
 vim.g["conjure#log#hud#enabled"] = false
 
@@ -27,7 +81,7 @@ return {
     -- https://github.com/CopilotC-Nvim/CopilotChat.nvim/blob/main/lua/CopilotChat/config.lua
     "CopilotC-Nvim/CopilotChat.nvim",
     opts = function(_, opts)
-      -- opts.model = "claude-3.5-sonnet"
+      opts.model = "claude-3.7-sonnet"
       --[[
       opts.prompts = {
         CustomPrompt = {
@@ -37,6 +91,7 @@ return {
           description = "My custom prompt description",
         },
       }--]]
+      opts.debug = true
       -- level "info" does not appear to create CopilogChat.log
       -- opts.log_level = "trace"
       -- opts.debug = false -- Enable debug logging (same as 'log_level = 'debug')
