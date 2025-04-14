0666495a311b2c55b81068f4728feafaf1e97f8e
Andreas Steffan <a.steffan@contentreich.de>
2025-04-14 08:30:58 +0200

    chore(nvim): Moving towards mcp

--- config/nvim/lua/plugins/random.lua
+++ config/nvim/lua/plugins/random.lua
@@ -1,3 +1,4 @@
+local log = require("plenary.log")
 -- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
 --
 -- In your plugin files, you can:
@@ -79,6 +80,10 @@ end
 
 return {
   {
+    "Olical/nfnl",
+    ft = "fennel",
+  },
+  {
     -- copilot-chat extra spec overrides
     -- https://github.com/CopilotC-Nvim/CopilotChat.nvim/discussions/420
     -- https://github.com/CopilotC-Nvim/CopilotChat.nvim/blob/main/lua/CopilotChat/config.lua
@@ -120,6 +125,120 @@ return {
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
+    },
+  },
+  {
+    "ravitemer/mcphub.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- Required for Job and HTTP requests
+    },
+    -- comment the following line to ensure hub will be ready at the earliest
+    cmd = "MCPHub", -- lazy load by default
+    build = "npm install -g mcp-hub@latest", -- Installs required mcp-hub npm module
+    -- uncomment this if you don't want mcp-hub to be available globally or can't use -g
+    -- build = "bundled_build.lua",  -- Use this and set use_bundled_binary = true in opts  (see Advanced configuration)
+    config = function()
+      require("mcphub").setup({
+        -- foo and bar or baz -> lua if expression
+        config = vim.fn.getenv("HOME") and vim.fn.expand("~/.config/mcphub/servers.json"), -- We may want this host specific, overriding the default
+        on_ready = function(hub)
+          local async = require("plenary.async")
+          local call_tool = async.wrap(function(server, tool, input, callback)
+            hub:call_tool(server, tool, input, {
+              callback = function(response)
+                callback(response)
+              end,
+            })
+          end, 4)
+
+          local tools = hub:get_tools()
+          local status, copilot_chat = pcall(require, "CopilotChat")
+
+          if not status then
+            -- Module couldn't be loaded
+            vim.notify("Couldn't load module_name: " .. copilot_chat, vim.log.levels.WARN)
+            -- my_module now contains the error message
+            return -- or provide fallback functionality
+          end
+          local chat = copilot_chat.chat
+          for _, tool in ipairs(tools) do
+            vim.print("Tool name: " .. tool.name)
+            -- log.debug("Tool name: " .. tool.name)
+            --[[
+            chat.config.tools[tool.name] = {
+              description = tool.description,
+              schema = tool.inputSchema,
+              resolve = function(input, source)
+                local out = call_tool(tool.server, tool.name, input)
+                vim.print(out)
+                return {}
+              end,
+            }
+            ]]
+            --
+          end
+        end,
+      })
+    end,
+  },
+  {
+    -- avante.nvim is a Neovim plugin designed to emulate the behaviour of the Cursor
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    version = false, -- Never set this value to "*"! Never!
+    opts = {
+      -- add any opts here
+      -- for example
+      provider = "openai",
+      openai = {
+        endpoint = "https://api.openai.com/v1",
+        model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
+        timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
+        temperature = 0,
+        max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+        --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+      },
+    },
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    build = "make",
+    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below dependencies are optional,
+      "echasnovski/mini.pick", -- for file_selector provider mini.pick
+      "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+      "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+      "ibhagwan/fzf-lua", -- for file_selector provider fzf
+      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      "zbirenbaum/copilot.lua", -- for providers='copilot'
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
     },
   },
   {
