8ed222ad87f9179b84553c3beda832f3e460b5f4
Andreas Steffan <a.steffan@contentreich.de>
2025-05-07 19:54:05 +0200

    feat(nvim): disabled vectorcode, added obsidian/coc

--- config/nvim/lua/plugins/random.lua
+++ config/nvim/lua/plugins/random.lua
@@ -310,6 +310,14 @@ return {
         },
       },
       --[[
+      display = {
+        diff = {
+          provider = "mini_diff", -- TODO: Workaround for https://github.com/olimorris/codecompanion.nvim/pull/1368
+        },
+      },
+      --]]
+      --
+      --[[
       strategies = {
         -- Change the default chat adapter
         chat = {
@@ -327,15 +335,31 @@ return {
             show_result_in_chat = true,
           },
         },
+        --[[
+        vectorcode = {
+          opts = {
+            add_tool = true,
+          },
+        },
+        ]]
+        --
       },
       opts = {
-        log_level = "DEBUG", -- TRACE|DEBUG|ERROR|INFO
+        log_level = "TRACE", -- TRACE|DEBUG|ERROR|INFO
       },
     },
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
     },
+  },
+  {
+    "Davidyz/VectorCode",
+    version = "*", -- optional, depending on whether you're on nightly or release
+    build = "uv tool install --upgrade vectorcode", -- pipx upgrade fails when it is not installed already
+    -- build = "pipx upgrade vectorcode", -- optional but recommended. This keeps your CLI up-to-date.
+    dependencies = { "nvim-lua/plenary.nvim" },
+    enabled = false,
   },
   {
     "ravitemer/mcphub.nvim",
@@ -485,5 +509,41 @@ return {
       end,
       ]]
     --
+  },
+  {
+    "neoclide/coc.nvim",
+    breanch = "release",
+  },
+  "epwalsh/obsidian.nvim",
+  version = "*", -- recommended, use latest release instead of latest commit
+  lazy = true,
+  ft = "markdown",
+  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+  -- event = {
+  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+  --   -- refer to `:h file-pattern` for more examples
+  --   "BufReadPre path/to/my-vault/*.md",
+  --   "BufNewFile path/to/my-vault/*.md",
+  -- },
+  dependencies = {
+    -- Required.
+    "nvim-lua/plenary.nvim",
+
+    -- see below for full list of optional dependencies 👇
+  },
+  opts = {
+    workspaces = {
+      {
+        name = "personal",
+        path = "~/vaults/personal",
+      },
+      {
+        name = "work",
+        path = "~/vaults/work",
+      },
+    },
+
+    -- see below for full list of options 👇
   },
 }
