e04756b7eaa4873a85481cb80f2c67c8eecf6f6c
Andreas Steffan <a.steffan@contentreich.de>
2025-06-01 16:38:58 +0200

    feat(nvim): Migrated vectorcode pipx -> uv

--- config/nvim/lua/plugins/random.lua
+++ config/nvim/lua/plugins/random.lua
@@ -390,10 +390,11 @@ return {
   {
     "Davidyz/VectorCode",
     version = "*", -- optional, depending on whether you're on nightly or release
-    -- TODO: : uv tool installs 6GB (mostly due to torch/cuda we don't need for non gpu version)
-    -- PIP_INDEX_URL="https://download.pytorch.org/whl/cpu" PIP_EXTRA_INDEX_URL="https://pypi.org/simple" pipx install vectorcode
     -- build = "uv tool install --upgrade vectorcode", -- pipx upgrade fails when it is not installed already
-    build = "pipx upgrade vectorcode", -- optional but recommended. This keeps your CLI up-to-date.
+    -- uv tool install vectorcode --index https://download.pytorch.org/whl/cpu
+    build = "uv tool upgrade vectorcode", -- This helps keeping the CLI up-to-date
+    -- PIP_INDEX_URL="https://download.pytorch.org/whl/cpu" PIP_EXTRA_INDEX_URL="https://pypi.org/simple" pipx install vectorcode
+    -- build = "pipx upgrade vectorcode", -- optional but recommended. This keeps your CLI up-to-date.
     dependencies = { "nvim-lua/plenary.nvim" },
     -- enabled = false,
   },
