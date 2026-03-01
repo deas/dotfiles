0f0a846cda8d516d35222cf494019b5fa13907a6
Andreas Steffan <a.steffan@contentreich.de>
2026-03-01 17:21:25 +0100

    chore(nvim): Plugin cleanup

--- config/nvim/lua/plugins/random.lua
+++ config/nvim/lua/plugins/random.lua
@@ -306,7 +306,7 @@ return {
           tools = {
             opts = {
               default_tools = {
-                "full_stack_dev",
+                "agent",
               },
             },
           },
@@ -339,14 +339,14 @@ return {
       },
       --]]
       extensions = {
-        mcphub = {
-          callback = "mcphub.extensions.codecompanion",
-          opts = {
-            make_vars = true,
-            make_slash_commands = true,
-            show_result_in_chat = true,
-          },
-        },
+        -- mcphub = {
+        --   callback = "mcphub.extensions.codecompanion",
+        --   opts = {
+        --     make_vars = true,
+        --     make_slash_commands = true,
+        --     show_result_in_chat = true,
+        --   },
+        -- },
         --[[
         vectorcode = {
           opts = {
@@ -379,7 +379,7 @@ return {
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
-      "ravitemer/mcphub.nvim",
+      -- "ravitemer/mcphub.nvim",
       "ravitemer/codecompanion-history.nvim",
       -- { "ravitemer/mcphub.nvim", branch = "deas-v18" },
       -- { "ravitemer/codecompanion-history.nvim", branch = "deas-v18" },
@@ -396,113 +396,42 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     enabled = false,
   },
-  {
-    "ravitemer/mcphub.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim", -- Required for Job and HTTP requests
-    },
-    -- comment the following line to ensure hub will be ready at the earliest
-    cmd = "MCPHub", -- lazy load by default
-    build = "npm install -g mcp-hub@latest", -- Installs required mcp-hub npm module
-    -- uncomment this if you don't want mcp-hub to be available globally or can't use -g
-    -- build = "bundled_build.lua",  -- Use this and set use_bundled_binary = true in opts  (see Advanced configuration)
-    -- opts = mcp_opts, -- Not what it appears to be - opts wont be mcp_opts in function config
-    -- https://github.com/ravitemer/mcphub.nvim/discussions/110
-    opts = {
-      config = vim.fn.expand("~/.config/mcphub/servers.json"),
-    },
-    -- },
-    --[[
-    config = function(opts)
-      -- vim.print(vim.inspect(opts))
-      -- require("mcphub").setup(vim.tbl_extend("force", opts, mcp_opts))
-      -- Detault goes to notifications and is very noisy
-      --[[
-      opts.log = {
-        -- (vim.log.levels.ERROR, WARN, INFO, DEBUG, TRACE)
-        level = vim.log.levels.WARN,
-        -- to_file = false,
-        -- file_path = nil,
-        -- prefix = "MCPHub",
-      }
-      require("mcphub").setup(vim.tbl_extend("force", opts, {
-        config = mcp_servers_config(),
-      }))
-    end,
-    ]]
-    --
-  },
-  {
-    "joshuavial/aider.nvim",
-    opts = {
-      -- your configuration comes here
-      -- if you don't want to use the default settings
-      auto_manage_context = true, -- automatically manage buffer context
-      default_bindings = true, -- use default <leader>A keybindings
-      debug = false, -- enable debug logging
-    },
-  },
-  {
-    -- avante.nvim is a Neovim plugin designed to emulate the behaviour of the Cursor
-    "yetone/avante.nvim",
-    enabled = false,
-    event = "VeryLazy",
-    version = false, -- Never set this value to "*"! Never!
-    opts = {
-      -- add any opts here
-      -- for example
-      provider = "openai",
-      openai = {
-        endpoint = "https://api.openai.com/v1",
-        model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
-        timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
-        temperature = 0,
-        max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
-        --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
-      },
-    },
-    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-    build = "make",
-    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "stevearc/dressing.nvim",
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      --- The below dependencies are optional,
-      "nvim-mini/mini.pick", -- for file_selector provider mini.pick
-      "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-      "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions -- TODO : Should be blink
-      "ibhagwan/fzf-lua", -- for file_selector provider fzf
-      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-      "zbirenbaum/copilot.lua", -- for providers='copilot'
-      {
-        -- support for image pasting
-        "HakonHarnes/img-clip.nvim",
-        event = "VeryLazy",
-        opts = {
-          -- recommended settings
-          default = {
-            embed_image_as_base64 = false,
-            prompt_for_file_name = false,
-            drag_and_drop = {
-              insert_mode = true,
-            },
-            -- required for Windows users
-            use_absolute_path = true,
-          },
-        },
-      },
-      {
-        -- Make sure to set this up properly if you have lazy=true
-        "MeanderingProgrammer/render-markdown.nvim",
-        opts = {
-          file_types = { "markdown", "Avante" },
-        },
-        ft = { "markdown", "Avante" },
-      },
-    },
-  },
+  -- {
+  --   "ravitemer/mcphub.nvim",
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim", -- Required for Job and HTTP requests
+  --   },
+  --   -- comment the following line to ensure hub will be ready at the earliest
+  --   cmd = "MCPHub", -- lazy load by default
+  --   build = "npm install -g mcp-hub@latest", -- Installs required mcp-hub npm module
+  --   -- uncomment this if you don't want mcp-hub to be available globally or can't use -g
+  --   -- build = "bundled_build.lua",  -- Use this and set use_bundled_binary = true in opts  (see Advanced configuration)
+  --   -- opts = mcp_opts, -- Not what it appears to be - opts wont be mcp_opts in function config
+  --   -- https://github.com/ravitemer/mcphub.nvim/discussions/110
+  --   opts = {
+  --     config = vim.fn.expand("~/.config/mcphub/servers.json"),
+  --   },
+  --   -- },
+  --   --[[
+  --   config = function(opts)
+  --     -- vim.print(vim.inspect(opts))
+  --     -- require("mcphub").setup(vim.tbl_extend("force", opts, mcp_opts))
+  --     -- Detault goes to notifications and is very noisy
+  --     --[[
+  --     opts.log = {
+  --       -- (vim.log.levels.ERROR, WARN, INFO, DEBUG, TRACE)
+  --       level = vim.log.levels.WARN,
+  --       -- to_file = false,
+  --       -- file_path = nil,
+  --       -- prefix = "MCPHub",
+  --     }
+  --     require("mcphub").setup(vim.tbl_extend("force", opts, {
+  --       config = mcp_servers_config(),
+  --     }))
+  --   end,
+  --   ]]
+  --   --
+  -- },
   {
     "mason-org/mason.nvim",
     opts = function(_, opts)
